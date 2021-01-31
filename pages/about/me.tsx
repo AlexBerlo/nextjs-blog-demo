c244388a14dbe5c4fddc940185a01483daace2fc
AlexBerlo <berlo1989@gmail.com>
2021-01-31 22:29:24 +0200

    Initial

---
+++ pages/about/me.tsx
@@ -0,0 +1,7 @@
+export default function Me() {
+  return (
+    <div>
+      <h1>hI i'M mE</h1>
+    </div>
+  )
+}
---
+++ pages/hello.tsx
@@ -0,0 +1,11 @@
+import {Article} from "../components/article";
+
+export default function Hello() {
+  return (
+    <Article>
+      <div>
+        <h1>Holla Mariola</h1>
+      </div>
+    </Article>
+  );
+}
